700a2794cdf57afb1a8db2496aa05b56ed4a7dae
Jose Gomez <k13gomez@gmail.com>
2019-07-23 19:30:27 -0400

    housekeeping cleanup

--- src/clj_native_lambda_poc/example.clj
+++ src/clj_native_lambda_poc/example.clj
@@ -11,14 +11,13 @@
 (def dynamodb-client
   (atom nil))
 
-(defn initialize!
-  []
-  (reset! dynamodb-client
-    (let [^EnvironmentVariableCredentialsProvider cred-provider (EnvironmentVariableCredentialsProvider/create)
-          ^DynamoDbClientBuilder builder (DynamoDbClient/builder)
-          ^DynamoDbClientBuilder with-http (.httpClient builder http-client)
-          ^DynamoDbClientBuilder with-cred (.credentialsProvider with-http cred-provider)]
-      (.build with-cred))))
+(defn initialize! []
+  (let [^EnvironmentVariableCredentialsProvider cred-provider (EnvironmentVariableCredentialsProvider/create)
+        dynamodb-client* (let [^DynamoDbClientBuilder builder (DynamoDbClient/builder)
+                               ^DynamoDbClientBuilder with-http (.httpClient builder http-client)
+                               ^DynamoDbClientBuilder with-cred (.credentialsProvider with-http cred-provider)]
+                           (.build with-cred))]
+    (reset! dynamodb-client dynamodb-client*)))
 
 (defn dynamodb-handler
   [input context]
